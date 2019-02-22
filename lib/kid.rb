6ffcaa5015defd6181a321b87d8f3bec9229134b
Carmen Renee <carmengenpt@gmail.com>
2019-02-22 13:37:30 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative '.fancy_dance.rb'
 
 class Kid 
-  include Dance
+  include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
   
   attr_accessor :name 
