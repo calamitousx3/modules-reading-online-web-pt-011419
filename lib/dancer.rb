20e2c4cbdaee7a9b8c5d3d1871d16d3f6ff4a888
Carmen Renee <carmengenpt@gmail.com>
2019-02-22 13:10:12 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  include Dance 
-  extend MetaDancing
+  include Dance #instance methods
+  extend MetaDancing #class methods
   
   attr_accessor :name 
   
