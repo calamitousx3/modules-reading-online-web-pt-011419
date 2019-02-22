9704b864ee91f1201bb8203e3277a826910a112d
Carmen Renee <carmengenpt@gmail.com>
2019-02-22 13:20:26 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative '.fancy_dance.rb'
 
 #class Dancer 
   #include Dance #instance methods
@@ -12,3 +13,7 @@ require_relative './dance_module.rb'
  # 
 # end 
  
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
